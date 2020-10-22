4bda3495c2e9938d1e54e3a75bad26b291896b87
Adam Syed <adam.syed@rogers.com>
2020-10-22 18:36:33 -0400

    Added template HTML Pages

---
+++ query.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="moviedatabase.cabnbngmbcmp.ca-central-1.rds.amazonaws.com",
+  port=3306,
+  user="admin",
+  password="msci342!",
+  database="moviedb"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("show tables;")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
