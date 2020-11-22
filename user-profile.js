e5920561ce85f400373bd39bb67fef34396e016f
Adam Syed <adam.syed@rogers.com>
2020-11-22 11:55:32 -0500

    Added the navbar to the group pages. Fixed a bug in the user-profile page

--- user-profile.js
+++ user-profile.js
@@ -35,21 +35,25 @@ async function update(){
     var email = document.info.email.value;
     var password = document.info.password.value;
 
-    const response = await fetch(API_URL.concat(api_endpoint), {
-        method: 'PUT',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify({firstname:firstname,lastname:lastname,email:email,password:password,id:vars.id})
-    });
-    const json = await response.json();
-    console.log(json.response);
-    
-    if (json.response == 'Success'){
-        document.getElementById('saved').innerHTML = "We've updated your profile!";
-    } else {
-        document.getElementById('saved').innerHTML = "We were unable to succesfully update your profile, please contact our support team.";
-    }
+    if (first_name == '' | last_name == '' | email == '' | password == ''){
+        document.getElementById('invalid').innerHTML = 'Please fill in all of the fields.';
+    } else{
+        const response = await fetch(API_URL.concat(api_endpoint), {
+            method: 'PUT',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({firstname:firstname,lastname:lastname,email:email,password:password,id:vars.id})
+        });
+        const json = await response.json();
+        console.log(json.response);
+        
+        if (json.response == 'Success'){
+            document.getElementById('saved').innerHTML = "We've updated your profile!";
+        } else {
+            document.getElementById('saved').innerHTML = "We were unable to succesfully update your profile, please contact our support team.";
+        }
+    }   
 }
 
 function redirect(page) {
