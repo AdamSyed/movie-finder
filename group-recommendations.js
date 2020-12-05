5e6949a7d2c4fdd6afaba7f433067ac80733f2b6
rrhabib <rrhabib@uwaterloo.ca>
2020-12-05 01:02:34 -0500

    created the template for the group recommendations html page.  Unit tested local instances of the javascrip method for this page and everything looks good so far.  Will have to deploy for testing of online links

--- group-recommendations.js
+++ group-recommendations.js
@@ -5,6 +5,7 @@ async function groupMovies() {
 
     // const RETURN_MOVIE_URL = 'https://cors-anywhere.herokuapp.com/http://moviefinder.us-east-1.elasticbeanstalk.com/rating/1';
     const RETURN_MOVIE_URL = 'https://cors-anywhere.herokuapp.com/http://moviefinder.us-east-1.elasticbeanstalk.com/group-results';
+    
     //console.log("reached here");
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -24,9 +25,19 @@ async function groupMovies() {
 
     const jsonFile = await response.json();
 
+    jsonFile = {
+        "top_name": "My Marks",
+        "top_genre": "Tragedy",
+        "second_name": "Murder on the Orient Express",
+        "second_genre": "Mystery",
+        "third_name": "Inception",
+        "third_genre": "Action",
+    };
+
 	console.log(jsonFile);
 
     //'top_name': out[0], 'top_id': out[1], 'top_genre': out[2],'second_name': out[3], 'second_id': out[4], 'second_genre': out[5],'third_name': out[6],'third_id': out[7],'third_genre': out[8],}
+        
 
     //assign the Javascript values to the approporate HTML sections for diplay
     document.getElementById('first').innerHTML = jsonFile["top_name"];
